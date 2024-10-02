287117f728d14269fc2e0cc770c2ef1b396b81a7
ricardomachado <ricardojtmachado@hotmail.com>
2024-10-02 22:30:34 +0100

    [FEAT] Changes

--- CarAuctionAPI/Data/AuctionDbContext.cs
+++ CarAuctionAPI/Data/AuctionDbContext.cs
@@ -9,14 +9,4 @@ public class AuctionDbContext : DbContext
 
     public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Auction> Auctions { get; set; }
-
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     modelBuilder.Entity<Vehicle>()
-    //         .HasDiscriminator<string>("VehicleType")
-    //         .HasValue<Hatchback>("Hatchback")
-    //         .HasValue<Sedan>("Sedan")
-    //         .HasValue<SUV>("SUV")
-    //         .HasValue<Truck>("Truck");
-    // }
 }
